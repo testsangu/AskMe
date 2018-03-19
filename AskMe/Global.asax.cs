ae024966d945550fa35194ad081c3d4303fb5b8d
sangamesh Arali <sangamesh.arali@happiestminds.com>
2018-03-19 09:37:44 +0530

    test

--- AskMe/Global.asax.cs
+++ AskMe/Global.asax.cs
@@ -15,6 +15,7 @@ namespace AskMe
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
